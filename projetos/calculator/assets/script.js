17f01081cc160c491a2cd9937a88f0d32fabe2c1
BMR23 <brenomelo161@hotmail.com>
2023-04-24 20:09:46 -0300

    2.0

--- projetos/calculator/assets/script.js
+++ projetos/calculator/assets/script.js
@@ -3,55 +3,54 @@ var display = document.getElementById('display');
 var subDisplay = document.getElementById('subDisplay');
 var contagemConclusao = 0;
 var numOperador = 0;
+var numOperadorEspecial = 0;
 
-/* function preCalculo(num){
-    if (num == 0){
-        if (display.textContent != ''){
-            preResultado = calcular()
-            displayTeste = document.getElementById('testeCalculadora')
-            displayTeste.textContent = preResultado
-        }
-    }
-    else{
-        return
-    }
-} */
 
 function inserirDigito(digito){
     console.log(digito);
     var display = document.getElementById('display');
     numOperador = 0;
+    numOperadorEspecial = 0;
     //preCalculo(numOperador)
 
 
     function remover(){
-        let text = display.textContent
-        let text2 = text.substring(0, text.length - 1)
-        display.innerHTML = text2
+        let text = display.textContent;
+        let text2 = text.substring(0, text.length - 1);
+        display.innerHTML = text2;
     }
 
     
-    if(digito == 'remove'){
-        remover()
-        return 0
+    if(digito == 'Backspace'){
+        remover();
+        return 0;
+    }
+    else if (digito == 'Enter'){
+        calcular();
+        return 0;
+    }
+    else if(digito == 'Delete'){
+        display.innerHTML = "";
+        subDisplay.innerHTML = "";
+        return 0 ;
     }
     else if(digito.length != 1){
-        return 0
+        return 0;
     }
     else{
-        numOperador = 0
+        numOperador = 0;
         if(contagemConclusao != 0){
-            display.textContent = ''
+            display.textContent = '';
         }
         let text = display.textContent
-        let virgula = text.includes(',')
-        contagemConclusao = 0
+        let virgula = text.includes(',');
+        contagemConclusao = 0;
 
         if (virgula == false){
-            display.innerHTML += digito
+            display.innerHTML += digito;
             //numOperador--
         }else if (digito != ','){
-            display.innerHTML += digito
+            display.innerHTML += digito;
             //numOperador--
         }
     }
@@ -63,148 +62,150 @@ function inserirDigito(digito){
 function inserirOperador(operador){
     var display = document.getElementById('display');
     var subDisplay = document.getElementById('subDisplay');
-
-    if (operador == 'finalizar'){
-        return
-    }else if(operador.length != 1){
-        return 0
-    }
-    contagemConclusao = 0
-
-    function limpar(){
-        display.innerHTML = ""
-        subDisplay.innerHTML = ""
-    }
-    /* function remover(){
-        let text = display.textContent
-        let text2 = text.substring(0, text.length - 2)
-        display.innerHTML = text2
-    } */
+    let operadores = ['+', '-', '*', 'x', '/', '÷', '√', '%', '^', '.', ',']
+    let operadoresNormais = ['+', '-']
+    let lastLetterDisplay = display.textContent.slice(-1)
     
-    if(operador == 'C'){
-        limpar()
-        return 0 
-    }
-    /* else if(numOperador != 0){
-        let text = display.textContent
-        text = text.replace(" ", '')
-        let text2 = text.substring(0, text.length - 1)
-        if (text2)
-        //display.innerHTML += operador
-        //display.innerHTML = operador
-        return 0
-    } */
-
-    else{
-        let lastLetterDisplay = display.textContent.slice(-1)
-        let condicao1 = lastLetterDisplay != '+' && lastLetterDisplay != '-'
-        let condicao2 = lastLetterDisplay != 'x' && lastLetterDisplay != '/'
-        let condicao3 = lastLetterDisplay != '÷' && lastLetterDisplay != '%'
-        
-        if (condicao1 && condicao2 && condicao3){
-            numOperador = 1
-            display.innerHTML += operador
-                //numOperador++
-    
+    // Se o operador estiver incluido na lista operadores
+    if (operadores.includes(operador)){
+        // Se a ultima letra do display não estiver incluido na lista operadores
+        if (! (operadores.includes(lastLetterDisplay) ) && lastLetterDisplay == ''){
+            if ( operadoresNormais.includes(operador) ){
+                contagemConclusao = 0;
+                numOperador = 1;
+                display.innerHTML += operador;
+            }
+            else{ return;}
+        }
+        else if (!(operadores.includes(lastLetterDisplay)) && !(lastLetterDisplay == '')){
+            contagemConclusao = 0;
+            numOperador = 1;
+            display.innerHTML += operador;
         }
     }
-} 
+    else if(operador == 'C'){
+        display.innerHTML = "";
+        subDisplay.innerHTML = "";
+        return 0 ;
+    }
+    else{ return 0; }
+}
+/* 
+    if (lastLetterDisplay == '%'){
+        if(operador != '%' && operador != '√'){
+            contagemConclusao = 0;
+            numOperador = 1;
+            display.innerHTML += operador;
+        }
+        else{ return; }
+    else if(operador == '%'){
+        if (!(isNaN(lastLetterDisplay))){
+            numOperadorEspecial = 1;
+            display.innerHTML += operador;
+        }
+        else{ return; }
+    }
+    else if(operador != '√'){
+        if (isNaN(lastLetterDisplay)){
+            numOperadorEspecial = 1;
+            display.innerHTML += operador;
+} */
+
+    
 
 function separaString(text){
-    let lista = []
-    let ind = 0
-    console.log("Função separaString:")
+    let lista = [];
+    let ind = 0;
 
     for (t of text){
         text = text.replace('%', '/100');
-        text = text.replace('x', '*')
-        text = text.replace('÷', '/')
+        text = text.replace('x', '*');
+        text = text.replace('÷', '/');
+        text = text.replace(',', '.');
     }
-    console.log('text depois')
-    console.log(text)
 
-    while(ind < text.length){
+    for(ind; ind <= text.length; ind ++){
+        /* if (text[ind] == '√'){
+            if(!(isNaN(ind-1)) && ind != 0){
+                lista.push('*');
+            }
+            let indZ = ind + 1;
+            for (indZ; indZ < text.length; indZ++){
+                if(!(isNaN(text[indZ])) || indZ == '.'){
+                    sub0 += text[indZ];
+                    //alert(text[indZ])
+                }
+                else{ break; }
+            } 
+            num = parseFloat(sub0);
+            resul = Math.sqrt(num);
+            //alert(indZ)
+            resul = resul + ''
+            lista.push(resul);
+            text = text.substring(indZ-1);
+            continue;
+        } */
         if (isNaN(text[ind])){
-            sub0 = text.substring(0,ind)
-            op = text[ind]
-            lista.push(sub0, op)
-            text = text.substring(ind+1)
-            ind = 0
-            continue
+            if (ind == 0){
+                op = text[ind];
+                lista.push('0', op)
+                text = text.substring(ind+1);
+                ind = 0;
+                continue;
+            }
+            sub0 = text.substring(0,ind);
+            op = text[ind];
+            lista.push(sub0, op);
+            text = text.substring(ind+1);
+            ind = 0;
+            continue;
         }
         else{
             if (!(isNaN(text))){
+                alert('o que sobrou do texto');
+                alert(text);
                 // Se o que sobrou do texto for numérico
                 if (isNaN(lista[lista. length - 1])){
                     // Se o último item da lista não for numérico,
                     // portanto, operador
-                    lista.push(text)
+                    lista.push(text);
                 }}
-            ind++
             continue
         }
-/*
-case '√':
-if(!(isNaN(ind-1))){
-    lista.push('*')
-}
-let indZ = ind + 1
-for (indZ; indZ < text.length; indZ++){
-    if(!(isNaN(text[indZ])) || indZ == '.'){
-        sub0 += text[indZ]
-        //alert(text[indZ])
     }
-    else{ break }
-    }
-sub0 = 
-num = parseFloat(sub0)
-resul = Math.sqrt(num)
-//alert(indZ)
-resul = resul + ''
-lista.push(resul)
-text = text.substring(indZ-1)
-continue
-
-*/
-    }
-    /* alert('lista depois:')
-    alert(lista) */
-    return lista
+    return lista;
 }
 
 function calcular(){
     var display = document.getElementById('display');
     var subDisplay = document.getElementById('subDisplay');
     let text = display.textContent;
-
-    /* if (subDisplay.textContent == 0){
-        subDisplay.innerHTML = text
-    }else {
-        subDisplay.innerHTML += `<br>${text}`
-    } */
-
-
-    text = text.replace(',', '.')
-    console.log('text antes do separaString')
-    console.log(text)
     let lista = separaString(text)
-   
+    let calc = 0
+    let contador = 0
+
     if (lista[0] == ''){
-        console.log('Entendeu que há um operador no início')
         nadaQuePreste = lista.shift()
     }
     if(isNaN(lista[lista.length-1]) || lista[lista.length-1] == ' '){
-        console.log("Entendeu que o último não é digito")
         lista.pop()
     }
-    console.log('Passou das verificações:')
-    console.log(lista)
-    let calc = 0
-    let contador = 0
+
     if (lista.length >= 3 ){
         while(contador === 0) {
 
-            if(lista.includes('*') && lista.includes('/')){
+            if(lista.includes('^')){
+                x = lista.indexOf('^')
+                num1 = parseFloat(lista[x-1])
+                num2 = parseFloat(lista[x+1])
+                calc = num1 ** num2
+                lista[x] = calc
+                lista.splice(x-1, 1)
+                lista.splice(x, 1)
+                continue
+            }
+
+            else if(lista.includes('*') && lista.includes('/')){
                 let indexMult = lista.indexOf('*')
                 let indexDivid = lista.indexOf('/')
 
@@ -316,7 +317,6 @@ function calcular(){
 }
 
 
-
 function seCliclar(){
     const digitos = document.getElementById('digitos');
     digitos.addEventListener("click", (digito)=>{
@@ -334,6 +334,20 @@ function seCliclar(){
 
     const finalizar = document.getElementById('finalizar');
     finalizar.addEventListener('click', calcular);
+
+    addEventListener('keydown', (event)=>{
+        // verificaTeclado(event.key)
+        tecla = event.key
+        if(tecla.length > 4){
+            inserirDigito(tecla)
+        }
+        else if (!(isNaN(tecla))){
+            inserirDigito(tecla)
+        }
+        else if (isNaN(tecla)){
+            inserirOperador(tecla)
+        }
+    })
 
 }
 
